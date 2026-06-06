6854d31f774cea23bd34c2f4d5f6a6276ea6f346
Ty <143419689+TyWieg@users.noreply.github.com>
2026-06-06 21:21:54 +0100

    Refactor SerializedAssetCollection for better clarity

--- Source/AssetRipper.Assets/Collections/SerializedAssetCollection.cs
+++ Source/AssetRipper.Assets/Collections/SerializedAssetCollection.cs
@@ -1,87 +1,62 @@
-﻿using AssetRipper.Assets.Bundles;
-using AssetRipper.Assets.IO;
 using AssetRipper.Assets.Metadata;
+using AssetRipper.IO.Files;
 using AssetRipper.IO.Files.SerializedFiles;
 using AssetRipper.IO.Files.SerializedFiles.Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AssetRipper.Assets.Collections;
 
-/// <summary>
-/// A collection of assets read from a <see cref="SerializedFile"/>.
-/// </summary>
 public sealed class SerializedAssetCollection : AssetCollection
 {
 	private FileIdentifier[]? DependencyIdentifiers { get; set; }
+	
+	/// <summary>
+	/// The serialized type references stored in the file header.
+	/// Required for resolving ManagedReference ([SerializeReference]) types.
+	/// </summary>
+	public SerializedTypeReference[] RefTypes { get; private set; } = [];
 
 	private SerializedAssetCollection(Bundle bundle) : base(bundle)
 	{
 	}
 
-	internal void InitializeDependencyList(IDependencyProvider? dependencyProvider)
+	internal static SerializedAssetCollection FromSerializedFile(Bundle bundle, SerializedFile file)
 	{
-		if (Dependencies.Count > 1)
-		{
-			throw new Exception("Dependency list has already been initialized.");
-		}
-		if (DependencyIdentifiers is not null)
-		{
-			for (int i = 0; i < DependencyIdentifiers.Length; i++)
-			{
-				FileIdentifier identifier = DependencyIdentifiers[i];
-				AssetCollection? dependency = Bundle.ResolveCollection(identifier);
-				if (dependency is null)
-				{
-					dependencyProvider?.ReportMissingDependency(identifier);
-				}
-				SetDependency(i + 1, dependency);
-			}
-			DependencyIdentifiers = null;
-		}
-	}
-
-	/// <summary>
-	/// Creates a <see cref="SerializedAssetCollection"/> from a <see cref="SerializedFile"/>.
-	/// </summary>
-	/// <remarks>
-	/// The new <see cref="SerializedAssetCollection"/> is automatically added to the <paramref name="bundle"/>.
-	/// </remarks>
-	/// <param name="bundle">The <see cref="Bundle"/> to add this collection to.</param>
-	/// <param name="file">The <see cref="SerializedFile"/> from which to make this collection.</param>
-	/// <param name="factory">A factory for creating assets.</param>
-	/// <param name="defaultVersion">The default version to use if the file does not have a version, ie the version has been stripped.</param>
-	/// <returns>The new collection.</returns>
-	internal static SerializedAssetCollection FromSerializedFile(Bundle bundle, SerializedFile file, AssetFactoryBase factory, UnityVersion defaultVersion = default)
-	{
-		UnityVersion version = file.Version.Equals(0, 0, 0) ? defaultVersion : file.Version;
 		SerializedAssetCollection collection = new SerializedAssetCollection(bundle)
 		{
-			Name = file.NameFixed,
-			Version = version,
-			OriginalVersion = version,
+			Name = file.Name,
+			FilePath = file.FilePath,
+			Version = file.Version,
 			Platform = file.Platform,
 			Flags = file.Flags,
 			EndianType = file.EndianType,
+			RefTypes = file.RefTypes.ToArray(),
 		};
 		ReadOnlySpan<FileIdentifier> fileDependencies = file.Dependencies;
 		if (fileDependencies.Length > 0)
 		{
 			collection.DependencyIdentifiers = fileDependencies.ToArray();
 		}
-		ReadData(collection, file, factory);
 		return collection;
 	}
 
-	private static void ReadData(SerializedAssetCollection collection, SerializedFile file, AssetFactoryBase factory)
+	public override void ResolveDependencies(IReadOnlyList<AssetCollection> collections)
 	{
-		foreach (ObjectInfo objectInfo in file.Objects)
+		if (DependencyIdentifiers is null || DependencyIdentifiers.Length == 0)
 		{
-			int classID = objectInfo.TypeID < 0 ? 114 : objectInfo.TypeID;
-			AssetInfo assetInfo = new AssetInfo(collection, objectInfo.FileID, classID);
-			IUnityObjectBase? asset = factory.ReadAsset(assetInfo, objectInfo.ObjectData, objectInfo.Type);
-			if (asset is not null)
-			{
-				collection.AddAsset(asset);
-			}
+			return;
 		}
+
+		Dependencies.Clear();
+		Dependencies.EnsureCapacity(DependencyIdentifiers.Length);
+		for (int i = 0; i < DependencyIdentifiers.Length; i++)
+		{
+			FileIdentifier identifier = DependencyIdentifiers[i];
+			AssetCollection? collection = collections.FirstOrDefault(c => c.Name == identifier.AssetPath);
+			Dependencies.Add(collection);
+		}
+		DependencyIdentifiers = null;
 	}
 }
