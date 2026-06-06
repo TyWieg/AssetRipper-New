4ee73fa1bf78df4250a889e867d8942dd4ba74a8
DeletedAim12 <143419689+DeletedAim12@users.noreply.github.com>
2026-06-06 02:11:50 +0100

    AssetRipper

---
+++ Source/AssetRipper.SourceGenerated.Extensions/SerializedShaderFloatValueExtensions.cs
@@ -0,0 +1,27 @@
+﻿using AssetRipper.SourceGenerated.Subclasses.SerializedShaderFloatValue;
+
+namespace AssetRipper.SourceGenerated.Extensions;
+
+public static class SerializedShaderFloatValueExtensions
+{
+	extension(ISerializedShaderFloatValue floatValue)
+	{
+		public bool IsZero() => floatValue.Value == 0.0f;
+		public bool IsMax() => floatValue.Value == 255.0f;
+		public Utf8String Name
+		{
+			get => floatValue.Has_Name_R_Utf8String() ? floatValue.Name_R_Utf8String : floatValue.Name_R_FastPropertyName.Name;
+			set
+			{
+				if (floatValue.Has_Name_R_Utf8String())
+				{
+					floatValue.Name_R_Utf8String = value;
+				}
+				else
+				{
+					floatValue.Name_R_FastPropertyName.Name = value;
+				}
+			}
+		}
+	}
+}
