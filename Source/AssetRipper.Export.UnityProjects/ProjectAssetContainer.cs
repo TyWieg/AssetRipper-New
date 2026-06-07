d1eecadb0ac6d251539c30243b589f8e67f88082
Ty <143419689+TyWieg@users.noreply.github.com>
2026-06-07 16:41:17 +0200

    Refactor ProjectAssetContainer for better asset management

--- Source/AssetRipper.Export.UnityProjects/ProjectAssetContainer.cs
+++ Source/AssetRipper.Export.UnityProjects/ProjectAssetContainer.cs
@@ -1,33 +1,33 @@
-using AssetRipper.Assets;
 using AssetRipper.Assets.Collections;
 using AssetRipper.Export.UnityProjects.Project;
 using AssetRipper.Import.Configuration;
+using AssetRipper.Import.Logging;
 using AssetRipper.Processing.Scenes;
 using AssetRipper.SourceGenerated.Classes.ClassID_141;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace AssetRipper.Export.UnityProjects;
 
-public class ProjectAssetContainer : IExportContainer
+public sealed class ProjectAssetContainer
 {
-	public ProjectAssetContainer(ProjectExporter exporter, CoreConfiguration options, IEnumerable<IUnityObjectBase> assets,
-		IReadOnlyList<IExportCollection> collections)
+	private readonly Dictionary<IUnityObjectBase, IExportCollection> m_assetCollections = new();
+	private readonly SceneExportCollection[] m_scenes;
+
+	public ProjectAssetContainer(ProjectExporter exporter, CoreConfiguration options, IEnumerable<IExportCollection> collections)
 	{
-		m_exporter = exporter ?? throw new ArgumentNullException(nameof(exporter));
-		CurrentCollection = null!;
-
-		ExportVersion = options.Version;
-
-		m_buildSettings = assets.OfType<IBuildSettings>().FirstOrDefault();
-
 		List<SceneExportCollection> scenes = new();
 		foreach (IExportCollection collection in collections)
 		{
 			foreach (IUnityObjectBase asset in collection.Assets)
 			{
-				CheckIfAlreadyAdded(this, asset, collection);
-				m_assetCollections.Add(asset, collection);
+				if (!m_assetCollections.TryAdd(asset, collection))
+				{
+					Logger.Warning(LogCategory.Export, $"Asset {asset} is already added by {m_assetCollections[asset]}. Skipping addition from {collection}.");
+				}
 			}
 			if (collection is SceneExportCollection scene)
 			{
@@ -35,63 +35,31 @@ public class ProjectAssetContainer : IExportContainer
 			}
 		}
 		m_scenes = scenes.ToArray();
-
-		[Conditional("DEBUG")]
-		static void CheckIfAlreadyAdded(ProjectAssetContainer container, IUnityObjectBase asset, IExportCollection currentCollection)
-		{
-			if (container.m_assetCollections.TryGetValue(asset, out IExportCollection? previousCollection))
-			{
-				throw new ArgumentException($"Asset {asset} is already added by {previousCollection}");
-			}
-		}
 	}
 
 	public long GetExportID(IUnityObjectBase asset)
 	{
 		if (m_assetCollections.TryGetValue(asset, out IExportCollection? collection))
 		{
-			return collection.GetExportID(this, asset);
+			return collection.GetExportID(asset);
 		}
-
-		return ExportIdHandler.GetMainExportID(asset);
+		throw new ArgumentException($"Asset {asset} is not inside any collection", nameof(asset));
 	}
 
-	public AssetType ToExportType(Type type)
-	{
-		return m_exporter.ToExportType(type);
-	}
-
-	public MetaPtr CreateExportPointer(IUnityObjectBase asset)
+	public IExportCollection GetCollection(IUnityObjectBase asset)
 	{
 		if (m_assetCollections.TryGetValue(asset, out IExportCollection? collection))
 		{
-			return collection.CreateExportPointer(this, asset, collection == CurrentCollection);
+			return collection;
 		}
-
-		return MetaPtr.CreateMissingReference(asset.ClassID, AssetType.Meta);
+		throw new ArgumentException($"Asset {asset} is not inside any collection", nameof(asset));
 	}
 
-	public UnityGuid ScenePathToGUID(string path)
+	public bool TryGetCollection(IUnityObjectBase asset, [NotNullWhen(true)] out IExportCollection? collection)
 	{
-		foreach (SceneExportCollection scene in m_scenes)
-		{
-			if (scene.Scene.Path == path)
-			{
-				return scene.GUID;
-			}
-		}
-		return default;
+		return m_assetCollections.TryGetValue(asset, out collection);
 	}
 
-	public bool IsSceneDuplicate(int sceneIndex) => SceneHelpers.IsSceneDuplicate(sceneIndex, m_buildSettings);
-
-	public IExportCollection CurrentCollection { get; set; }
-	public AssetCollection File => CurrentCollection.File;
-	public UnityVersion ExportVersion { get; }
-
-	private readonly ProjectExporter m_exporter;
-	private readonly Dictionary<IUnityObjectBase, IExportCollection> m_assetCollections = new();
-
-	private readonly IBuildSettings? m_buildSettings;
-	private readonly SceneExportCollection[] m_scenes;
+	public IReadOnlyCollection<IExportCollection> Collections => m_assetCollections.Values.Distinct().ToArray();
+	public IReadOnlyCollection<SceneExportCollection> Scenes => m_scenes;
 }
